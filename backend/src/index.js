768b05b4b80deaf3504228c6fceed5643e9f47e7
Aryan Swarnkar <Aryan Swarnkar>
2026-04-20 01:21:19 +0530

    fix render port issue

--- backend/src/index.js
+++ backend/src/index.js
@@ -21,7 +21,8 @@ connectDB();
 
 // Middleware
 app.use(cors({
-  origin: process.env.FRONTEND_URL || 'http://localhost:3000',
+  origin: process.env.FRONTEND_URL || 'https://bookverse-snowy.vercel.app/',
+
   credentials: true,
 }));
 
